7274bfc6bc6a265d3d3ec582f40a967854b87619
Tung <nguyen.tung@outlook.de>
2022-10-11 09:30:29 +0200

    W/L calculation

--- javascript.js
+++ javascript.js
@@ -1,46 +1,34 @@
 
-    var table, rows, switching, i, x, y, shouldSwitch;
-    table = document.getElementById("eastern-conference-table");
 
-    rows = table.rows;
-    for (i = 1; 1 < (rows.length -1); i++) {
-        var w = (rows[i].getElementsByTagName("TD")[0]);
-        var l = ((rows[i].getElementsByTagName("TD")[1]));
-        var wl = w / l;
-        rows[i].getElementsByTagName("TD")[2].innerHTML = (wl);
-      //  console.log(rows[i].getElementsByTagName("TD")[1]);
-      console.log(rows)
+// W/L berechnen
+var table, rows, tauschen, i, x, y, mussTauschen;
+table = document.getElementById("eastern-conference-table");
+
+for (i = 1; i < (table.rows.length); i++) {
+  table.rows[i].cells[3].innerHTML = (Number(table.rows[i].cells[1].outerText) / (Number(table.rows[i].cells[1].outerText) + Number(table.rows[i].cells[2].outerText))).toFixed(2)
+}
+
+
+// Tabelle sortieren
+rows = table.rows;
+tauschen = true;
+while (tauschen) {
+  tauschen = false;
+  rows = table.rows;
+
+  for (i = 1; i < (rows.length - 1); i++) {
+    mussTauschen = false;
+    x = rows[i].getElementsByTagName("TD")[2];
+    y = rows[i + 1].getElementsByTagName("TD")[2];
+
+    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+      mussTauschen = true;
+      break;
     }
+  }
 
-
-// Sort Table
-    switching = true;
-    /*Make a loop that will continue until
-    no switching has been done:*/
-    while (switching) {
-      //start by saying: no switching is done:
-      switching = false;
-      rows = table.rows;
-      /*Loop through all table rows (except the
-      first, which contains table headers):*/
-      for (i = 1; i < (rows.length - 1); i++) {
-        //start by saying there should be no switching:
-        shouldSwitch = false;
-        /*Get the two elements you want to compare,
-        one from current row and one from the next:*/
-        x = rows[i].getElementsByTagName("TD")[2];
-        y = rows[i + 1].getElementsByTagName("TD")[2];
-        //check if the two rows should switch place:
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      }
-      if (shouldSwitch) {
-        /*If a switch has been marked, make the switch
-        and mark that a switch has been done:*/
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-      }
-    }
+  if (mussTauschen) {
+    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+    tauschen = true;
+  }
+}
