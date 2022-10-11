4ca96485488b14f156ccf09e35c77dfb956560a1
Tung <nguyen.tung@outlook.de>
2022-10-11 14:09:36 +0200

    icons

--- javascript.js
+++ javascript.js
@@ -1,13 +1,13 @@
 
-
-// W/L berechnen
 var table, rows, tauschen, i, x, y, mussTauschen;
+
+// Eastern Conference Table
 table = document.getElementById("eastern-conference-table");
 
+// W/L-Berechnung
 for (i = 1; i < (table.rows.length); i++) {
-  table.rows[i].cells[3].innerHTML = (Number(table.rows[i].cells[1].outerText) / (Number(table.rows[i].cells[1].outerText) + Number(table.rows[i].cells[2].outerText))).toFixed(2)
+  table.rows[i].cells[3].innerHTML = (Number(table.rows[i].cells[1].outerText) / (Number(table.rows[i].cells[1].outerText) + Number(table.rows[i].cells[2].outerText))).toFixed(2);
 }
-
 
 // Tabelle sortieren
 rows = table.rows;
@@ -18,8 +18,8 @@ while (tauschen) {
 
   for (i = 1; i < (rows.length - 1); i++) {
     mussTauschen = false;
-    x = rows[i].getElementsByTagName("TD")[2];
-    y = rows[i + 1].getElementsByTagName("TD")[2];
+    x = rows[i].getElementsByTagName("TD")[3];
+    y = rows[i + 1].getElementsByTagName("TD")[3];
 
     if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
       mussTauschen = true;
@@ -32,3 +32,48 @@ while (tauschen) {
     tauschen = true;
   }
 }
+
+// GB-Berechnung
+for (i = 1; i < (table.rows.length); i++) {  
+  table.rows[i].cells[4].innerHTML = ((Number(table.rows[1].cells[1].outerText) - (Number(table.rows[1].cells[2].outerText))) - (Number(table.rows[i].cells[1].outerText) - (Number(table.rows[i].cells[2].outerText)))) / 2;
+}
+table.rows[1].cells[4].innerHTML = "-";
+
+
+// Western Conference Table
+table = document.getElementById("western-conference-table");
+
+// W/L-Berechnung
+for (i = 1; i < (table.rows.length); i++) {
+  table.rows[i].cells[3].innerHTML = (Number(table.rows[i].cells[1].outerText) / (Number(table.rows[i].cells[1].outerText) + Number(table.rows[i].cells[2].outerText))).toFixed(2);
+}
+
+// Tabelle sortieren
+rows = table.rows;
+tauschen = true;
+while (tauschen) {
+  tauschen = false;
+  rows = table.rows;
+
+  for (i = 1; i < (rows.length - 1); i++) {
+    mussTauschen = false;
+    x = rows[i].getElementsByTagName("TD")[3];
+    y = rows[i + 1].getElementsByTagName("TD")[3];
+
+    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+      mussTauschen = true;
+      break;
+    }
+  }
+
+  if (mussTauschen) {
+    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+    tauschen = true;
+  }
+}
+
+// GB-Berechnung
+for (i = 1; i < (table.rows.length); i++) {  
+  table.rows[i].cells[4].innerHTML = ((Number(table.rows[1].cells[1].outerText) - (Number(table.rows[1].cells[2].outerText))) - (Number(table.rows[i].cells[1].outerText) - (Number(table.rows[i].cells[2].outerText)))) / 2;
+}
+table.rows[1].cells[4].innerHTML = "-";
